93adf2f4f6677d57c8723af452e5504ced46ad1b
Fatima Mubasher <147225995+FatimaMubasher03@users.noreply.github.com>
2024-11-28 22:33:50 +0500

    Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int int_value;
+    long long_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    
+    scanf("%d",&int_value);
+    printf("%d\n",int_value);
+    scanf("%ld",&long_value);
+    printf("%ld\n",long_value);
+    scanf(" %c",&char_value);
+    printf("%c\n",char_value);
+    scanf("%f",&float_value);
+    printf("%f\n",float_value);
+    scanf("%lf",&double_value);
+    printf("%lf\n",double_value);
+    return 0;
+}
